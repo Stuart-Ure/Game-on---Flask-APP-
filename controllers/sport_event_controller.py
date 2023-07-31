188b335026d0ca2a9982ee37e1947f51f9c52e3b
Stuart <stuartrure@gmail.com>
2023-07-31 15:15:08 +0100

    forms and buttons added

--- controllers/sport_event_controller.py
+++ controllers/sport_event_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from models.sport_event import SportEvent
 from models.user import User
 from models.visits import Visit
+from app import db
 
 sport_event_blueprint = Blueprint("sport_event", __name__)
 
@@ -18,11 +19,27 @@ def sport_events():
 #     users = User.query.join(Visit).filter(Visit.sport_event_id ==id)
 #     return render_template("sportevents/show.jinja", sport_event = sport_event, users =users)
 
+#HERE I AM ALLOWING THE SPORT EVENT INSTANCE TO BE SEARCHED BY ID FROM THE DATABASE
 
 @sport_event_blueprint.route("/sportevents/<int:id>")
 def show(id):
     sport_event = SportEvent.query.get(id)
-    if sport_event is None:
-        return render_template("errors/404.jinja"), 404
+    # if sport_event is None:
+    #     return render_template("errors/404.jinja"), 404
 
     return render_template("sportevents/show.jinja", sport_event=sport_event)
+
+
+
+@sport_event_blueprint.route("/sportevents", methods=['POST'])
+def create_sport_event():
+    name = request.form.get('name')
+    sport = request.form.get('sport')
+    location = request.form.get('location')
+    date = request.form.get('date')
+
+    new_sport_event = SportEvent(name=name, sport=sport, location=location, date=date)
+    db.session.add(new_sport_event)
+    db.session.commit()
+
+    return redirect('/sportevents')
